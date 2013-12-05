b8c56f0c244c2f250051b110b2317b78ca1e1757
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-05 15:29:03 -0500

    call from anywhere

--- convert.sh
+++ convert.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pandoc "content/$1.md" -f markdown -t odt -o "odt/$1.odt"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pandoc "$DIR/content/$1.md" -f markdown -t odt -o "$DIR/odt/$1.odt"
