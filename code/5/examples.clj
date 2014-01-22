71ec73335cf286a569103aaee1931baf2a4e88c3
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-22 14:51:21 -0500

    blargity

--- code/5/examples.clj
+++ code/5/examples.clj
@@ -5,11 +5,9 @@
       "hocus pocus"))
 
 
-(let [
-      final-body-parts ]
-  (if (has-matching-part? (first remaining-asym-parts))
-    (recur (rest remaining-asym-parts)
-           (conj (conj (conj final-body-parts part) (first remaining-asym-parts))
-                 (matching-part (first remaining-asym-parts))))
-    (recur (rest remaining-asym-parts)
-           (conj (conj final-body-parts part) (first remaining-asym-parts)))))
+(if (has-matching-part? (first remaining-asym-parts))
+  (recur (rest remaining-asym-parts)
+         (conj (conj (conj final-body-parts part) (first remaining-asym-parts))
+               (matching-part (first remaining-asym-parts))))
+  (recur (rest remaining-asym-parts)
+         (conj (conj final-body-parts part) (first remaining-asym-parts))))
