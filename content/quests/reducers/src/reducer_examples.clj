9a6b6d80eee5c1827b1f98a4e0a8aadb79223e99
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2016-01-03 12:02:03 -0500

    more progress

--- content/quests/reducers/src/reducer_examples.clj
+++ content/quests/reducers/src/reducer_examples.clj
@@ -1,16 +1,70 @@
 (ns reducer-examples
   (:require [clojure.core.reducers :as r]))
 
-(def nums (range 10000000))
+;;----- Just for printing out times for easy comparison
+(defn pretty-time
+  [expr]
+  `(let [start# (. System (nanoTime))
+         runs#  4]
+     (dotimes [n# runs#] ~expr)
+     (prn (format "%8.2f %s"
+                  (/ (double (- (. System (nanoTime)) start#)) 1000000.0 runs#)
+                  (quote ~expr)))))
 
-(time (r/fold + nums))
-(time (r/reduce + nums))
-(time (reduce + nums))
+(defmacro times
+  [& exprs]
+  `(do ~@(map pretty-time exprs)))
 
-(time (r/fold + (r/map inc nums)))
-(time (r/reduce + (r/map inc nums)))
-(time (reduce + (map inc nums)))
+;;----- Simple operations
+(def snums  (range 10000000))
+(def snumsv (vec snums))
 
-(time (r/fold + (r/map inc (r/filter even? nums))))
-(time (r/reduce + (r/map inc (r/filter even? nums))))
-(time (reduce + (map inc (filter even? nums))))
+(defn t1 [x]
+  (times (r/fold + x)
+         (r/reduce + x)
+         (reduce + x)))
+
+(defn t2 [x]
+  (times (->> x (r/map inc) (r/fold +))
+         (->> x (r/map inc) (r/reduce +))
+         (->> x (pmap inc)  (reduce +))
+         (->> x (map inc)   (reduce +))))
+
+(defn t3 [x]
+  (times (->> x (r/filter even?) (r/map inc) (r/fold +))
+         (->> x (r/filter even?) (r/map inc) (r/reduce +))
+         (->> x (filter even?)   (pmap inc)  (reduce +))
+         (->> x (filter even?)   (map inc)   (reduce +))))
+
+
+;;----- More computationally intensive ops
+(def pnums  (range 10000))
+(def pnumsv (vec pnums))
+
+(defn primes [n]
+  (if (zero? n)
+    #{}
+    (loop [n n
+           factor 2
+           factors #{}]
+      (if (= n 1)
+        factors
+        (if (zero? (mod n factor))
+          (recur (/ n factor) factor (conj factors factor))
+          (recur n (+ factor 1) factors))))))
+
+(defn add-primes [n]
+  (reduce + (primes n)))
+
+(defn p1 [x]
+  (times (->> x (r/map add-primes) (r/fold +))
+         (->> x (r/map add-primes) (r/reduce +))
+         (->> x (pmap add-primes)  (reduce +))
+         (->> x (map add-primes)   (reduce +))))
+
+(defn p2 [x]
+  (times (->> x (r/filter even?) (r/map add-primes) (r/fold +))
+         (->> x (r/filter even?) (r/map add-primes) (r/reduce +))
+         (->> x (filter even?)   (pmap add-primes)  (reduce +))
+         (->> x (filter even?)   (map add-primes)   (reduce +))))
+;;-----
